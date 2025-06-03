bcc3da2dfc9f23c0bb4af22ec0fb3ff5af68ec1c
PotoVic <wiktor.okonski39@gmail.com>
2025-06-03 10:46:14 +0200

    feat: TodosPresentation component with edit state and input handling

--- src/components/TodoPresentation.tsx
+++ src/components/TodoPresentation.tsx
@@ -3,9 +3,24 @@ import { useState, type ChangeEvent } from "react";
 
 type TodoPresentationProps = {
     todo: Todo;
-
+    editTodo: (id: number) => void;
+    updateTodo:(t: Todo) => void;
+    removeTodo: (id: number) => void;
 }
 
-export const TodosPresentation = ({
+
+export const TodosPresentation = ({todo,editTodo,updateTodo,removeTodo}: TodoPresentationProps) => {
+    const [editedTodo, setEditedTodo] = useState(todo);
     
-})
+    const handleChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => {
+        const {id, value, type } = e.target;
+
+        setEditedTodo({
+            ...editedTodo,[id]: type === "checkbox" ? (e.target as HTMLInputElement).checked : type === "number" ? Number(value) : value,
+        });
+    };
+        
+    
+}
